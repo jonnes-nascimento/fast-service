fa9867448eedc2be36a8cda5590fa2a32fc06bc8
Jonnes Nascimento <jonnesfn@ipt.br>
2024-08-13 10:59:28 -0300

    Ajsutes nos scripts de inicialização do banco de dados e no arquivo do Docker Compose

--- dataset/init.sql
+++ dataset/init.sql
@@ -1,2 +1,10 @@
-create database "fast-service";
-
+CREATE DATABASE "fast-service"
+    WITH
+    TEMPLATE = template0
+    OWNER = "fast-service-user"
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Portuguese_Brazil.1252'
+    LC_CTYPE = 'Portuguese_Brazil.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
